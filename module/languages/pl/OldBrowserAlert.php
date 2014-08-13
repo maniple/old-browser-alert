7b44cd73223533350458575542e38d396ae68cb2
xemlock <xemlock@gmail.com>
2014-08-13 14:34:51 +0200

    pl translation update

--- module/languages/pl/OldBrowserAlert.php
+++ module/languages/pl/OldBrowserAlert.php
@@ -1,6 +1,6 @@
 <?php return array(
-    'You are using a browser from the past!' => 'Używasz średniowiecznej przeglądarki!',
-    'Our website is designed to work with <strong>modern web browsers</strong>. Using your current browser will prevent you from accessing many features on our website.' => 'Nasza strona działa poprawnie w <strong>nowoczesnych przeglądarkach internetowych</strong>. Używana przez Ciebie przeglądarka nie obsługuje wielu funkcjonalności dostępnych na stronie.',
+    'You are using a browser from the past!' => 'Używasz przedpotopowej przeglądarki!',
+    'Our website is designed to work with <strong>modern web browsers</strong>. Using your current browser will prevent you from accessing many features on our website.' => 'Nasza strona działa poprawnie w <strong>nowoczesnych przeglądarkach internetowych</strong>. Używana przez Ciebie przeglądarka nie obsługuje wielu funkcjonalności dostępnych na stronie, albo obsługuje je w błedny sposób.',
     'Use the links below to download a new browser or upgrade your existing browser.' => 'Skorzystaj z odnośników poniżej aby pobrać nową przeglądarkę lub zaktualizować obecną.',
     'By closing this window you acknowledge that your experience on our website may be degraded.' => 'Zamykając okienko akceptujesz ograniczoną funkcjonalność naszej strony.',
     'Show me the page anyway' => 'Mimo wszystko pokaż mi stronę',
