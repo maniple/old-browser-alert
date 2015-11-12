a52c1eac1ed8298053a790e091d9ce7bbc541faf
xemlock <xemlock@gmail.com>
2015-11-12 21:43:41 +0100

    updated module config

--- Module.php
+++ Module.php
@@ -7,12 +7,14 @@ class Module
     public function getConfig()
     {
         return array(
-            'view' => array(
-                'helperPath' => array(
-                    'OldBrowserAlert_View_Helper_' => __DIR__ . '/module/views/helpers/',
-                ),
-                'scriptPath' => array(
-                    __DIR__ . '/module/views/scripts',
+            'resources' => array(
+                'view' => array(
+                    'helperPath' => array(
+                        'OldBrowserAlert_View_Helper_' => __DIR__ . '/module/views/helpers/',
+                    ),
+                    'scriptPath' => array(
+                        __DIR__ . '/module/views/scripts',
+                    ),
                 ),
             ),
         );
