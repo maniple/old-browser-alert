64b88adf34d0b06d800725e6c2ab25169c63ee4d
xemlock <xemlock@gmail.com>
2017-04-01 13:59:52 +0200

    Refactor legacy bootstrapping code

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,5 +2,26 @@
 
 class OldBrowserAlert_Bootstrap extends Maniple_Application_Module_Bootstrap
 {
-    protected $_moduleTasks = array('translations');
+    public function getModuleDependencies()
+    {
+        return array();
+    }
+
+    public function getTranslationsConfig()
+    {
+        return array(
+            'scan'    => Zend_Translate::LOCALE_DIRECTORY,
+            'content' => dirname(__FILE__) . '/languages',
+        );
+    }
+
+    public function getViewConfig()
+    {
+        return array(
+            'scriptPaths' => dirname(__FILE__) . '/views/scripts',
+            'helperPaths' => array(
+                'OldBrowserAlert_View_Helper_' => dirname(__FILE__) . '/library/View/Helper/',
+            ),
+        );
+    }
 }
